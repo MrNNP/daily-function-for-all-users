7f726cca53bca1d35ce019efdb96f15f106e30f3
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2020-12-21 09:02:53 -0500

    add once daily system

---
+++ main.js
@@ -0,0 +1,35 @@
+var users = [];
+
+// get data from db, and put into list
+// example:  db.get('userlist',users);
+
+const userObject = function (userId, score){
+    this.id = userId,
+    this.score = score,
+    this.increment = function(){
+    score++;    
+    }
+    users.push(this);
+}
+
+const incrementUserScores = function (userList){
+    userList.forEach(function (user){
+        user.increment;
+    });
+}
+
+var user1 = new userObject(101,23);
+
+var user2 = new userObject(102,83);
+
+var user3 = new userObject(103,43);
+
+
+incrementUserScores(users);
+users.forEach(function (user){
+    console.log(user.score)
+});
+
+// uncomment for it to run every 24 hours
+//setInterval(8.64e+7, incrementUserScores(users));
+
