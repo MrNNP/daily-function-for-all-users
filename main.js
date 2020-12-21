bc87a190a5bb2853edb19908d807f9f4b0b7c836
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2020-12-21 09:26:19 -0500

    added per user increment depending if date different

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ var users = [];
 // get data from db, and put into list
 // example:  db.get('userlist',users);
 
-const userObject = function (userId, score){
+const userObject = function (userId, score, lastActivity = new Date()){
     this.id = userId,
     this.score = score,
+    this.lastActivity = lastActivity.toISOString().slice(0, 10),
     this.increment = function(){
     this.score++;    
     }
@@ -24,17 +25,24 @@ var user2 = new userObject(102,83);
 
 var user3 = new userObject(103,43);
 
-console.log(users);
+function runWhenUserActivity(user){
+    let currentDate = new Date().toISOString().slice(0, 10)
+    if (user.lastActivity >= currentDate){
+        return;
+    } else {
+        user.increment();
+        user.lastActivity = currentDate;
+    }
 
-incrementUserScores(users);
-
-console.log(users);
-
-
-users.forEach(function (user){
-    console.log(user.score)
-});
+}
 
 // uncomment for it to run every 24 hours
 //setInterval(8.64e+7, incrementUserScores(users));
+
+
+//example 
+runWhenUserActivity(user1);
+console.log(user1);
+
+
 
